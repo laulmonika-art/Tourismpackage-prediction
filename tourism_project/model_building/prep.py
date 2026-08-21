40d8a463cdb95b6084fadb9675fc2edd5d6a398c
laulmonika-art <laul.monika@gmail.com>
2026-08-22 00:39:50 +0530

    update tourism_project/model_building/prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -10,9 +10,9 @@ from sklearn.model_selection import train_test_split
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
-DATA_PATH = PROJECT_ROOT / "data" / "tourism.csv"
+DATA_PATH = PROJECT_ROOT/"data"/"tourism.csv"
 
-OUTPUT_DIR = PROJECT_ROOT / "data" / "processed"
+OUTPUT_DIR = PROJECT_ROOT/"data"/"processed"
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 
